5e122bf490925353097678a8287b3bc6836771fe
unknown <minhnghiale1590@gmail.com>
2022-12-01 14:01:32 +0700

    Thread added to server.py

--- server.py
+++ server.py
@@ -24,6 +24,36 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 server_socket.listen(5)
 print('Start Server...')
+
+def server_login(mess):
+    cur.execute("select * from user where username=%s and password = %s and status = 1",
+                (message["user_name"], message["password"]))
+    row = cur.fetchone()
+
+    if row == None:
+        text = "Not"
+        connection_socket.send(text.encode())
+    else:
+        cur.execute("UPDATE user SET IP = %s WHERE username = %s and password = %s", (message["ip"], message["user_name"], message["password"]))
+        text = "Ok"
+        connection_socket.send(text.encode())
+
+def server_show(mess):
+    cur.execute("select id, name, IP, image from user")
+    rows = cur.fetchall()
+    lists = [list(x) for x in rows]
+    jsonStr = json.dumps(lists)
+
+    # data = message.encode()
+    # connection_socket.send(data)
+    send_text(connection_socket, jsonStr)
+
+def server_signup(mess):
+    # cur.execute("select ")
+    cur.execute("INSERT INTO user (name, username, password, IP, status) values (%s, %s, %s, %s, 1)", (message["name"], message["user_name"], message["password"], message["ip"]))
+    con.commit()
+    print("Sign up successfully!!")
+
 while 1:
     connection_socket, addr = server_socket.accept()
 
@@ -37,31 +67,14 @@ while 1:
     print(message["method"])
 
     if message["method"] == "login":
-        cur.execute("select * from user where username=%s and password = %s and status = 1",
-                    (message["user_name"], message["password"]))
-        row = cur.fetchone()
-
-        if row == None:
-            text = "Not"
-            connection_socket.send(text.encode())
-        else:
-            cur.execute("UPDATE user SET IP = %s WHERE username = %s and password = %s", (message["ip"], message["user_name"], message["password"]))
-            text = "Ok"
-            connection_socket.send(text.encode())
+        loginThread=threading.Thread(target=server_login, args=(message,))
+        loginThread.start()
     elif message["method"] == "show":
-        cur.execute("select id, name, IP, image from user")
-        rows = cur.fetchall()
-        lists = [list(x) for x in rows]
-        jsonStr = json.dumps(lists)
-
-        # data = message.encode()
-        # connection_socket.send(data)
-        send_text(connection_socket, jsonStr)
-    else:
-        # cur.execute("select ")
-        cur.execute("INSERT INTO user (name, username, password, IP, status) values (%s, %s, %s, %s, 1)", (message["name"], message["user_name"], message["password"], message["ip"]))
-        con.commit()
-        print("Sign up successfully!!")
+        showThread=threading.Thread(target=server_show, args=(message,))
+        showThread.start()
+    elif message["method"] == "signup":
+        signupThread=threading.Thread(target=server_signup, args=(message,))
+        signupThread.start()
     
 
 connection_socket.close()
