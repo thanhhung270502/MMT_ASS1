f82096e80ac298739d0280b65166ec7805b500e1
unknown <minhnghiale1590@gmail.com>
2022-12-01 14:40:55 +0700

    Link Sign up

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,21 @@
-import threading
-import time
 
-def fun(i,j):
-    for i in range(i,j):
-        print("Hi")
-        time.sleep(1)
+            msg = bytes(f"{len(msg):<{HEADER_LENGTH}}", "utf-8") + msg
+            
+            client_socket.send(msg)
 
-thread=threading.Thread(target=fun, args=(4,6))
-thread.start()
+            text = client_socket.recv(1024)
+            response = text.decode()
+            print(response) 
+
+            if (response == "Not"):
+                mess = QMessageBox()
+                mess.setIcon(QMessageBox.Warning)
+                mess.setText("Invalid User Name And Password")
+                mess.exec_()
+            else:
+                self.LogIn.close()
+                call(["python", "mainchat.py",response,self.Username.text()])
+
+            client_socket.close()
+
+            print("End Client....")
